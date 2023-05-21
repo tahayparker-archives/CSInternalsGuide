411cafb4b692d280ed3cec4d0b4201439a1f237b
Taha Yaseen <98612931+tahayparker@users.noreply.github.com>
2023-05-21 12:30:59 +0000

    update website

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-$(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        $(this).toggleClass('active');
-    });
+$(document).ready(function() {
+	$('#sidebarCollapse').on('click', function() {
+		$('#sidebar').toggleClass('active');
+		$(this).toggleClass('active');
+	});
 });
 
-$("[data-trigger]").on("click", function(){
-    var trigger_id =  $(this).attr('data-trigger');
-    $(trigger_id).toggleClass("show");
-    $('body').toggleClass("offcanvas-active");
+$("[data-trigger]").on("click", function() {
+	var trigger_id = $(this).attr('data-trigger');
+	$(trigger_id).toggleClass("show");
+	$('body').toggleClass("offcanvas-active");
 });
 
 // close button 
-$(".btn-close").click(function(e){
-    $(".navbar-collapse").removeClass("show");
-    $("body").removeClass("offcanvas-active");
-}); 
+$(".btn-close").click(function(e) {
+	$(".navbar-collapse").removeClass("show");
+	$("body").removeClass("offcanvas-active");
+});
