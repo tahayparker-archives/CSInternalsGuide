6d2cc042c24108a3fd9ccd8903a9855ba08d1c77
Taha Yaseen <98612931+tahayparker@users.noreply.github.com>
2023-06-01 16:10:00 +0000

    Clean code

--- contact_script.js
+++ contact_script.js
@@ -2,16 +2,17 @@ const shareButtons = document.querySelectorAll('.tile-share-button')
 console.log(shareButtons)
 
 async function copyText(e) {
-//prevent button going to the site
-    e.preventDefault()
-    const link = this.getAttribute('link')
-    console.log(link)
-    try {
-        await navigator.clipboard.writeText(link)
-    } catch (err) {
-        console.error(err)
-    }
+	//prevent button going to the site
+	e.preventDefault()
+	const link = this.getAttribute('link')
+	console.log(link)
+	try {
+		await navigator.clipboard.writeText(link)
+	} catch (err) {
+		console.error(err)
+	}
 }
 
 shareButtons.forEach(shareButton =>
-    shareButton.addEventListener('click', copyText))
+	
+	shareButton.addEventListener('click', copyText))
